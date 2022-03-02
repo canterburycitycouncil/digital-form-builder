ffd7af0460c8ac4d2eb3d7b1d604f19f6510533e
Luke Zigler <luke.zigler@canterbury.gov.uk>
2022-03-02 11:39:12 +0000

    added upload and delete functions to data context

--- designer/client/context/DataContext.ts
+++ designer/client/context/DataContext.ts
@@ -4,7 +4,11 @@ import { FormDefinition } from "@xgovformbuilder/model";
 export const DataContext = createContext<{
   data: FormDefinition;
   save: (toUpdate: FormDefinition) => Promise<false>;
+  upload: (toUpdate: FormDefinition) => Promise<false>;
+  deleteForm: () => Promise<false>;
 }>({
   data: {} as FormDefinition,
   save: async (_data: FormDefinition) => false,
+  upload: async (_data: FormDefinition) => false,
+  deleteForm: async () => false,
 });
