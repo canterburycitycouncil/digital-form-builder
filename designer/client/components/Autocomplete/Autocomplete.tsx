7a2e480480fc0c65c05e0c23e66553454f75df15
Luke Zigler <luke.zigler@canterbury.gov.uk>
2022-03-24 10:50:24 +0000

    updated autocomplete field to use new govuk input component

--- designer/client/components/Autocomplete/Autocomplete.tsx
+++ designer/client/components/Autocomplete/Autocomplete.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { ComponentContext } from "../../reducers/component/componentReducer";
 import { Actions } from "../../reducers/component/types";
-import { i18n } from "../../i18n";
+import { GovUKInput } from "../govuk-fields";
 
 export function Autocomplete() {
   const { state, dispatch } = useContext(ComponentContext);
@@ -9,27 +9,39 @@ export function Autocomplete() {
   const { options = {} } = selectedComponent;
 
   return (
-    <div className="govuk-form-group">
-      <label
-        className="govuk-label govuk-label--s"
-        htmlFor="field-options-autocomplete"
-      >
-        {i18n("common.autocomplete.title")}
-      </label>
-      <span className="govuk-hint">{i18n("common.autocomplete.helpText")}</span>
-      <input
-        className="govuk-input"
-        id="field-options-autocomplete"
-        name="options.autocomplete"
-        type="text"
-        value={options.autocomplete || ""}
-        onChange={(e) =>
-          dispatch({
-            type: Actions.EDIT_OPTIONS_AUTOCOMPLETE,
-            payload: e.target.value,
-          })
-        }
-      />
-    </div>
+    <GovUKInput
+      translationNamespace="common"
+      fieldName="autocomplete"
+      fieldParent="options"
+      value={options.autocomplete || ""}
+      handleChange={(e) =>
+        dispatch({
+          type: Actions.EDIT_OPTIONS_AUTOCOMPLETE,
+          payload: e.target.value,
+        })
+      }
+    />
+    // <div className="govuk-form-group">
+    //   <label
+    //     className="govuk-label govuk-label--s"
+    //     htmlFor="field-options-autocomplete"
+    //   >
+    //     {i18n("common.autocomplete.title")}
+    //   </label>
+    //   <span className="govuk-hint">{i18n("common.autocomplete.helpText")}</span>
+    //   <input
+    //     className="govuk-input"
+    //     id="field-options-autocomplete"
+    //     name="options.autocomplete"
+    //     type="text"
+    //     value={options.autocomplete || ""}
+    //     onChange={(e) =>
+    //       dispatch({
+    //         type: Actions.EDIT_OPTIONS_AUTOCOMPLETE,
+    //         payload: e.target.value,
+    //       })
+    //     }
+    //   />
+    // </div>
   );
 }
