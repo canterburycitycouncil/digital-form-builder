a85d1ac99b9c7160bc61f294a6faa9d0bc266cf2
Luke Zigler <luke.zigler@canterbury.gov.uk>
2022-03-24 09:07:44 +0000

    updated data context so upload and delete are optional

--- designer/client/context/DataContext.ts
+++ designer/client/context/DataContext.ts
@@ -4,8 +4,8 @@ import { FormDefinition } from "@xgovformbuilder/model";
 export const DataContext = createContext<{
   data: FormDefinition;
   save: (toUpdate: FormDefinition) => Promise<false>;
-  upload: (toUpdate: FormDefinition) => Promise<false>;
-  deleteForm: () => Promise<false>;
+  upload?: (toUpdate: FormDefinition) => Promise<false>;
+  deleteForm?: () => Promise<false>;
 }>({
   data: {} as FormDefinition,
   save: async (_data: FormDefinition) => false,
