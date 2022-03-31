638f400962e1db9c5ff40039e78bfce4c9f2400c
Luke Zigler <luke.zigler@canterbury.gov.uk>
2022-03-31 13:54:33 +0100

    added submissionApi class new class is used for communicating with server to retrive submission data

---
+++ designer/client/api/submissionApi.ts
@@ -0,0 +1,107 @@
+import { IntegrationLog } from "../../server/plugins/routes/types";
+import { Submission, SubmissionKey } from "../submissions/types";
+
+interface AllSubmissionsResponse {
+  submissions: Submission[];
+  lastKey?: SubmissionKey;
+}
+
+export class SubmissionApi {
+  async getSubmissionsForForm(
+    formId: string,
+    subType: string,
+    lastEvaluatedKey?: SubmissionKey
+  ): Promise<AllSubmissionsResponse> {
+    return new Promise((resolve, reject) => {
+      const url = `/api/${formId}/submissions?subType=${subType}${
+        lastEvaluatedKey ? "&lastKey=$lastEvaluatedKey" : ""
+      }`;
+      window
+        .fetch(url, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        .then((res) => res.json())
+        .then((res) => {
+          let submissionRes: AllSubmissionsResponse = {
+            submissions: res.submissions as Submission[],
+          };
+          if (res.LastEvaluatedKey) {
+            submissionRes.lastKey = res.lastEvaluatedKey;
+          }
+          resolve(submissionRes);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async getSubmission(
+    formId: string,
+    submissionId: string
+  ): Promise<Submission> {
+    const url = `/api/${formId}/submissions/${submissionId}`;
+
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          resolve(res as Submission);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async getIntegrationLogs(
+    formId: string,
+    submissionId: string
+  ): Promise<IntegrationLog[]> {
+    const url = `/api/${formId}/submissions/${submissionId}/integration-logs`;
+
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          resolve(res as IntegrationLog[]);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async getIntegrationLog(
+    formId: string,
+    submissionId: string,
+    integrationId: string
+  ): Promise<IntegrationLog> {
+    const url = `/api/${formId}/submissions/${submissionId}/integration-logs/${integrationId}`;
+
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          resolve(res as IntegrationLog);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+}
