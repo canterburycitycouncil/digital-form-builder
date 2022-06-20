5ebbda3eca87a441260b93ba492f6915ed1e3c7e
Luke Zigler <luke.zigler@canterbury.gov.uk>
2022-06-20 13:32:28 +0100

    added new types for the logic expression builder

--- designer/client/components/LogicExpressions/types.ts
+++ designer/client/components/LogicExpressions/types.ts
@@ -1,4 +1,8 @@
-import { FormDefinition, LogicExpression } from "@xgovformbuilder/model/src";
+import {
+  FormDefinition,
+  LogicExpression,
+  Page,
+} from "@xgovformbuilder/model/src";
 
 export interface LogicExpressionProps {
   data: FormDefinition;
@@ -7,3 +11,27 @@ export interface LogicExpressionProps {
   onEdit: Function;
   onCancel: Function;
 }
+
+export const formVariables = [
+  "id",
+  "internalOnly",
+  "startPage",
+  "version",
+  "createdAt",
+  "updatedAt",
+];
+
+export const getLogicVariables = (
+  logicExpressions: LogicExpression[]
+): string[] => {
+  return logicExpressions.map((expression) => expression.variableName);
+};
+
+export const getComponentVariables = (pages: Page[]): string[] => {
+  return pages.reduce<string[]>((acc, page) => {
+    if (page.components && page.components.length > 0) {
+      acc.concat(page.components.map((component) => component.name));
+    }
+    return acc;
+  }, []);
+};
