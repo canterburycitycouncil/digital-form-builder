b04370d01506712abb061e1b69aaa00194ad3adf
Luke Zigler <luke.zigler@canterbury.gov.uk>
2022-08-04 09:22:29 +0100

    added new outputs to output library index file

--- designer/server/lib/outputs/index.ts
+++ designer/server/lib/outputs/index.ts
@@ -1,5 +1,15 @@
 import { freshdesk } from "./freshdesk";
-import { webhook } from "./webhook";
+import { GOVNotifySendEmail } from "./notify";
 import { s3fileupload } from "./s3fileupload";
+import { topdesk } from "./topdesk";
+import { topdeskIncident } from "./topdesk-incident";
+import { webhook } from "./webhook";
 
-export { freshdesk, webhook, s3fileupload };
+export {
+  freshdesk,
+  webhook,
+  s3fileupload,
+  topdesk,
+  topdeskIncident,
+  GOVNotifySendEmail,
+};
